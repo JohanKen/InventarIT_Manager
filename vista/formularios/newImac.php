e4e5204287e9924ccf54498e69e33f99afe08c80
Guillermo Mancera <guillermo.memo05@outlook.com>
2024-02-01 14:28:15 -0600

    Registro iMac

--- vista/formularios/newImac.php
+++ vista/formularios/newImac.php
@@ -28,7 +28,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro iMac</title>
-    <link rel="stylesheet" href="estilos/estilosFormularios.css">
+    
 </head>
 
 <body>
