d17bf9d8e8d49c9f34dd9d58a1220a1ae1d14544
Guillermo Mancera <guillermo.memo05@outlook.com>
2024-02-13 15:42:09 -0600

    Herramientas

--- vista/herramientas.php
+++ vista/herramientas.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="estilos/estilosColaboradores.css">
+    <link rel="stylesheet" href="estilos/estilosColaboradores.css"><! se tiene que quitar esta linea o modificar>
     <style>
        
     </style>
